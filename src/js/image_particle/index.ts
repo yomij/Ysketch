c8a5a1e141cbb5e4dc27c863e9a49fd57e69d147
shengyongming <sps-shengym@supaur.com>
2020-04-04 15:57:56 +0800

    style: :art::art::art:

--- src/js/image_particle/index.ts
+++ src/js/image_particle/index.ts
@@ -4,27 +4,27 @@ import ImageLoader from "./image_loader";
 
 const elephant = require('../../static/elephant.png').default;
 
-export default class ImageParticleRender implements YRender {
-	readonly canvas: HTMLCanvasElement;
-	readonly renderer: THREE.WebGLRenderer;
-	readonly scene: THREE.Scene;
-	readonly camera: THREE.PerspectiveCamera;
+export default class ImageParticleRender implements YRender{
+    readonly canvas: HTMLCanvasElement;
+    readonly renderer: THREE.WebGLRenderer;
+    readonly scene: THREE.Scene;
+    readonly camera: THREE.PerspectiveCamera;
+    constructor (id: string) {
+        this.canvas = document.getElementById(id) as HTMLCanvasElement;
+        this.renderer = new THREE.WebGLRenderer({
+            antialias: false,
+            canvas: this.canvas,
+        });
+        this.scene = new THREE.Scene()
+        this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
+        this.renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+    init(): ImageParticleRender {
+    		const img = new ImageLoader(elephant, 400, 400)
+				console.log(img)
 
-	constructor(id: string) {
-		this.canvas = document.getElementById(id) as HTMLCanvasElement;
-		this.renderer = new THREE.WebGLRenderer({
-			antialias: false,
-			canvas: this.canvas,
-		});
-		this.scene = new THREE.Scene()
-		this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000)
-		this.renderer.setSize(window.innerWidth, window.innerHeight);
-	}
 
-	init(): ImageParticleRender {
-		const img = new ImageLoader(elephant, 400, 400)
-		console.log(img)
-		return this
-	}
+        return this
+    }
 }
 
